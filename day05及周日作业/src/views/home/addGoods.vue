ec51206cbdbfabe4406170d62af821ebf7922bf1
songzengjie <511997036@qq.com>
2022-11-24 08:33:57 +0800

    xiugai

--- day05及周日作业/src/views/home/addGoods.vue
+++ day05及周日作业/src/views/home/addGoods.vue
@@ -1,43 +1,104 @@
 <template>
   <div>
-    <h4>选择食品种类</h4>
-        
-        <el-collapse v-model="activeNames" @change="handleChange">
-  <el-collapse-item title="一致性 Consistency" name="1">
-    <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-    <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-  </el-collapse-item>
-</el-collapse>
- </div>
+      添加商品
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="食品名称" prop="name">
+              <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="食品活动" prop="dpdz">
+              <el-input v-model="ruleForm.dpdz"></el-input>
+          </el-form-item>
+          <el-form-item label="食品详情" prop="phone">
+              <el-input v-model="ruleForm.phone"></el-input>
+          </el-form-item>
+          <el-form-item label="上传食品图片">
+              <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
+                  :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+          </el-form-item>
+          <el-form-item label="食品特点">
+              <el-select v-model="ruleForm.region" placeholder="请选择">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+          </el-form-item>
+          <el-form-item label="食品规格">
+              <el-radio-group v-model="ruleForm.resource">
+                  <el-radio label="单规格"></el-radio>
+                  <el-radio label="多规格"></el-radio>
+              </el-radio-group>
+          </el-form-item>
+          <el-form-item label="包装费">
+              <el-input-number v-model="ruleForm.num" @change="handleChange" :min="1" label="描述文字"></el-input-number>
+          </el-form-item>
+
+          <el-form-item label="多规格">
+              <el-input-number v-model="ruleForm.num1" @change="handleChange" :min="1" label="描述文字"></el-input-number>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+          </el-form-item>
+      </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-
-    }
+  data() {
+      return {
+          ruleForm: {
+              name: '',
+              xxdz: '',
+              phone: '',
+              dpjs: '',
+              dpby: '',
+              delivery: true,
+              delivery2: true,
+              delivery3: true,
+              delivery4: true,
+              delivery5: true,
+              delivery6: true,
+              num: 1,
+              num1: 1,
+              date1: '',
+              date2: '',
+              region: '',
+              resource: '',
+          },
+          rules: {
+              name: [{ required: true, message: "请输入店铺名称", trigger: "blur" }],
+              dpdz: [{ required: true, message: "请输入详细地址", trigger: "blur" }],
+              phone: [{ required: true, message: "请输入联系电话", trigger: "blur" }],
+          },
+          options: [],
+          imageUrl: '',
+          tableDataa: []
+      };
   },
   methods: {
+      submitForm() {
 
-  },
-  created () {
+      },
+      handleChange() {
 
-  },
-  mounted () {
+      },
+      handleAvatarSuccess() {
 
-  },
-  components: {
+      },
+      beforeAvatarUpload() {
 
+      }
   },
-  computed: {
-
-  },
-  watch: {
-
-  },
-}
+  created() {
+  }
+};
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+.aa {
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
--- day05及周日作业/src/views/home/adminSet.vue
+++ day05及周日作业/src/views/home/adminSet.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    管理员设置
+  <div class="set">
+      <h4>管理员信息</h4>
+      <div class="cont">
+        <p><span>姓名:</span>4444444555</p>
+        <p><span>注册时间:</span>2022-11-18 11:07</p>
+        <p><span>管理员权限:</span>管理员</p>
+        <p><span>管理员ID:</span>124071</p>
+        <p><span>更换头像:</span></p>
+        <p>
+          <el-upload
+        class="avatar-uploader"
+    action="https://jsonplaceholder.typicode.com/posts/"
+    :show-file-list="false"
+    :on-success="handleAvatarSuccess"
+     :before-upload="beforeAvatarUpload">
+  <img v-if="image" :src="image" class="avatar">
+  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+        </p>
+      </div>
  </div>
 </template>
 
@@ -8,10 +26,30 @@
 export default {
   data () {
     return {
+      image:'8576827.jpg'
 
     }
   },
   methods: {
+    handleAvatarSuccess(res, file) {
+        this.image = URL.createObjectURL(file.raw);
+        console.log(file);
+        
+      },
+      beforeAvatarUpload(file) {
+        console.log(file);
+        this.image = file.name
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      },
 
   },
   created () {
@@ -33,4 +71,23 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.set{
+  width: 100%;
+  height: 100%;
+}
+h4{
+  width: 100%;
+  text-align: center;
+}
+
+.cont{
+  width: 800px;
+  height: 500px;
+  margin: 20px auto;
+  background: #eee;
+  padding: 20px;
+  p{
+    line-height: 50px;
+  }
+}
 </style>
