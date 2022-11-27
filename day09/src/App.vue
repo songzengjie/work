39094b7dbe41fc202f585cae606df805d44d7935
songzengjie <511997036@qq.com>
2022-11-27 21:46:56 +0800

    day09

---
+++ day09/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+</style>
